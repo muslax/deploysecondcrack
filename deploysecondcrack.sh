423e46444e6c6c39c6639410f0d8edb8685f1d8c
muslax <muslax@gmail.com>
2015-09-19 03:04:50 +0700

    Change things related to Apache, PHP, etc.
    Change permission commands; exclude iptables, Apache and PHP; add simple guide at the end of script. Also change secondcrack repository to this source.

--- deploysecondcrack.sh
+++ deploysecondcrack.sh
@@ -1,45 +1,51 @@
 # Install Apache, PHP, Python, Git, and dependancies
 
-sudo yum install -y httpd php php-process python git gcc automake autoconf libtool make curl-devel libgcc.i686 glibc.i686 php-xml php-mbstring;
+#sudo yum install -y httpd php php-process python git gcc automake autoconf libtool make curl-devel libgcc.i686 glibc.i686 php-xml php-mbstring;
 
 # Start Apache on boot and turn off firewall
 
-sudo chkconfig  httpd on;
-sudo chkconfig iptables off;
+#sudo chkconfig  httpd on;
+#sudo chkconfig iptables off;
 
 # Install inotify-tools to make updating speedy
 
 wget --no-check-certificate http://github.com/downloads/rvoicilas/inotify-tools/inotify-tools-3.14.tar.gz; 
-  tar xvfz inotify-tools-3.14.tar.gz;
-  cd inotify-tools-3.14;
-  ./configure --prefix=/usr --libdir=/lib64;
-  sudo make;
-  sudo make install;
-  cd ~ ; sudo rm inotify-tools-3.14.tar.gz;
+tar xvfz inotify-tools-3.14.tar.gz;
+cd inotify-tools-3.14;
+./configure --prefix=/usr --libdir=/lib64;
+sudo make;
+sudo make install;
+cd ~ ;
+#sudo rm inotify-tools-3.14.tar.gz;
+clear;
 
 # Install start-stop-daemon for use in /etc/init.d/dropbox
 
 cd ~ ; wget http://developer.axis.com/download/distribution/apps-sys-utils-start-stop-daemon-IR1_9_18-2.tar.gz;
-  tar zxvf apps-sys-utils-start-stop-daemon-IR1_9_18-2.tar.gz;
-  cd apps/sys-utils/start-stop-daemon-IR1_9_18-2/;
-  sudo gcc start-stop-daemon.c -o start-stop-daemon;
-  cd ~ ; sudo rm apps-sys-utils-start-stop-daemon-IR1_9_18-2.tar.gz
+tar zxvf apps-sys-utils-start-stop-daemon-IR1_9_18-2.tar.gz;
+cd apps/sys-utils/start-stop-daemon-IR1_9_18-2/;
+sudo gcc start-stop-daemon.c -o start-stop-daemon;
+cd ~ ;
+#sudo rm apps-sys-utils-start-stop-daemon-IR1_9_18-2.tar.gz
+clear;
 
 # Install deploysecondcrack
 
 cd ~;
-sudo git clone git://github.com/nickwynja/deploysecondcrack.git;
+sudo git clone git://github.com/muslax/deploysecondcrack.git;
+clear;
 
 # Install Dropbox
     
 cd ~ && wget -O - http://www.dropbox.com/download?plat=lnx.x86_64 | tar xzf -
 sudo mkdir -p ~/Dropbox;
-sudo chown -R blog ~/Dropbox;
+sudo chown -R $USER:$USER ~/Dropbox;
 sudo chmod -R u+rw ~/Dropbox;
 sudo mkdir -p ~/.dropbox;
-sudo chown -R blog ~/.dropbox;
+sudo chown -R $USER:$USER ~/.dropbox;
 sudo chmod -R u+rw ~/.dropbox;
 sudo chmod -R o+x  ~/Dropbox;
+clear;
 
 # Install Dropbox service
 
@@ -48,12 +54,14 @@ sudo cp ~/deploysecondcrack/config-files/sysconfig-dropbox /etc/sysconfig/dropbo
 sudo chmod 755 /etc/init.d/dropbox
 sudo chkconfig --add dropbox;
 sudo chkconfig dropbox on;
+clear;
 
 # Install Dropbox CLI
 
 mkdir -p ~/bin;
 sudo wget -O /usr/bin/dropbox "http://www.dropbox.com/download?dl=packages/dropbox.py";
 sudo chmod 755 /usr/bin/dropbox;
+clear;
 
 # Set up Second Crack `update` cron
   
@@ -61,19 +69,20 @@ sudo crontab ~/deploysecondcrack/config-files/crontab.example;
 
 # Config Apache with default settings
 
-sudo cp ~/deploysecondcrack/config-files/httpd.conf /etc/httpd/conf/httpd.conf;
-sudo service iptables stop;
-sudo rm /etc/httpd/conf.d/welcome.conf;
+#sudo cp ~/deploysecondcrack/config-files/httpd.conf /etc/httpd/conf/httpd.conf;
+#sudo service iptables stop;
+#sudo rm /etc/httpd/conf.d/welcome.conf;
 sudo chmod o+x ~;
 
 # Config PHP settings for short_open_tags
 
-sudo cp ~/deploysecondcrack/config-files/php.ini /etc/php.ini;
+#sudo cp ~/deploysecondcrack/config-files/php.ini /etc/php.ini;
 
 # Install Second Crack
 
 cd ~/ ;
-sudo git clone git://github.com/marcoarment/secondcrack.git;
+sudo git clone git://github.com/muslax/secondcrack.git;
+clear;
 
 # Configure Second Crack
 
@@ -90,3 +99,17 @@ sudo chown -R blog:blog ~/Dropbox/;
 sudo chown -R blog:blog ~/deploysecondcrack;
 sudo cp ~/deploysecondcrack/config-files/config.php.example ~/secondcrack/config.php;
 sudo vi ~/secondcrack/config.php;
+clear;
+
+echo "1. Make sure Apache DocumentRoot is properly set to /home/blog/secondcrack/www";
+echo "2. Edit /etc/php.ini to enable short_open_tags";
+echo "3. Edit ~/.dropbox-dist/dropboxd; add \"export LANGUAGE=en\" (whithout quote) after first line.";
+echo "4. If directory assets isn't accessible, try \"sudo chmod -R o+x  ~/Dropbox;\"";
+echo "5. Restart httpd";
+
+# Manual
+# 1. Make sure Apache DocumentRoot is properly set to /home/blog/secondcrack/www
+# 2. Edit /etc/php.ini to enable short_open_tags
+# 3. Edit ~/.dropbox-dist/dropboxd; add "export LANGUAGE=en" (whithout quote) after first line.
+# 4. If directory assets isn't accessible, try "sudo chmod -R o+x  ~/Dropbox;"
+# 5. Restart httpd
